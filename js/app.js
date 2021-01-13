6f0c5af5b6e7b950ce08db63f349910cf8bbec3b
Brendan Smith <brendan5320@gmail.com>
2021-01-13 14:21:44 -0800

    fixed all the bugs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Image constructor
 function Image (url, title, description, keyword, horns) {
   this.image_url = url;
   this.title = title;
@@ -8,6 +9,7 @@ function Image (url, title, description, keyword, horns) {
   this.horns = horns;
 }
 
+// Render image prototype
 Image.prototype.render = function () {
   const $imageclone = $('li:first-child').clone();
   const $h2 = $imageclone.find('h2');
@@ -15,35 +17,37 @@ Image.prototype.render = function () {
   $imageclone.find('img').attr('src', this.image_url);
   $imageclone.find('img').attr('alt', this.description);
   $imageclone.find('p').text(this.description);
+  $imageclone.find('h3').text(this.keyword);
   $h2.addClass(this.keyword);
   $('ul').append($imageclone);
 }
-Image.prototype.menu = function () {
-  // if ($('select:contains("rhino")')){
-  //   break;
-  // }
+//Render Menu options
+Image.prototype.renderMenu = function () {
+  if ( $(`select:contains("${this.keyword}")`).length > 0){
+    return;
+  }
   const $menuClone = $('option:first-child').clone();
   $menuClone.attr('value', this.keyword);
   $menuClone.html(this.keyword);
   $('select').append($menuClone);
 }
 
+// Ajax functions
 $.ajax('data/page-1.json').then(returnData => {
   const horns = [];
   returnData.forEach(horn => {
     horns.push(new Image(horn.image_url, horn.title, horn.description, horn.keyword, horn.horns));
   });
   horns.forEach(horn => {
-    console.log(horn)
     horn.render();
-    horn.menu();
+    horn.renderMenu();
   });
 
 });
 
 // filter menu
-
-$('select').on('change', () => {
+$('select').on('change', function(e) {
   $('li').hide();
-  $('li:contains(unicorn)').show();
+  $('li:contains('+ e.target.value +')').show();
+  console.log(e.target.value);
 });
